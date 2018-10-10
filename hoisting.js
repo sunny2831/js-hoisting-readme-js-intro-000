0fd2abff4e2122c213db8e79425ce3559adb1710
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-10 19:41:58 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,20 +1,14 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
-
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+function thisIsCrazy() {
     console.log("hey!!!")
-  }
 }
 
 function sayMyName() {
@@ -23,8 +17,6 @@ function sayMyName() {
 
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+  function sayMyName() {
+    console.log(Kristin);
 }
